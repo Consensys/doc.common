885c636e1cbe5e596c9f84c01c3056ff8860dbc0
Nicolas MASSART <nicolas.massart@consensys.net>
2019-11-27 22:18:48 +0100

    add common build tools files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# doc.common
+# Pegasys common doc repository
+
 Common code and guidelines for all Pegasys product documentations
+
+This repository is to be included as a submodule on each doc site.
---
+++ build_tools/scripts/test_lint.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+circleci config process common/build_tools/circleci//config.yml > common/build_tools/circleci//process.yml && circleci local execute -c common/build_tools/circleci//process.yml --job markdownlint
